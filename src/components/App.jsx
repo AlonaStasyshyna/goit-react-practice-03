96d41ec731d7a63f245bc678c715dbc9f1664306
AlonaStasyshyna <alena.stasishyna28@gmail.com>
2022-10-18 10:46:20 +0300

    Add files of practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
+import {useState, useEffect} from 'react'
+import { EventsList } from './EventsList/EventsList';
+import { fetchApi } from 'fetchApi/fetchApi';
+
 export const App = () => {
+  const [events, setEvents] = useState([]);
+  const [isShown, setIsShown] = useState(false);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    if (isShown) {
+      fetchApi(page).then(resp => setEvents(prevEvents => 
+        [...prevEvents, ...resp.data._embedded.events]))
+    }
+  }, [isShown, page])
+
+  const showEvents = () => {
+    setIsShown(true)
+  }
+
+  const loadMore = () => {
+    setPage(prevPage => prevPage + 1)
+  }
+
+  const deleteEvent = (eventId) => {
+    setEvents((prevEvents) =>
+      prevEvents.filter(event => event.id !== eventId))
+  }
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+    <>
+      {isShown && <EventsList events={events} deleteEvent={deleteEvent} />}
+      {!isShown && (<button type='button' onClick={showEvents}>Show events</button>)}
+      {isShown && (<button type='button' onClick={loadMore}>Load more</button>)}
+    </>
+  )
+}
